bbcf804446490a2d22cfcd50c080a7dd97f6425e
henrynh2105 <hnh2105@gmail.com>
2025-09-24 22:22:10 -0500

    Initializes empty tables matching our UML diagram from Phase 1

---
+++ initializeTables.sql
@@ -0,0 +1,61 @@
+CREATE TABLE IF NOT EXISTS "MenuItem" (
+  "id" INT PRIMARY KEY,
+  "name" VARCHAR,
+  "price" DECIMAL
+);
+
+CREATE TABLE IF NOT EXISTS "Order" (
+  "id" INT PRIMARY KEY,
+  "date" DATETIME,
+  "price" DECIMAL,
+  "employeeId" INT,
+  "customerId" INT
+    CONSTRAINT "FK_Order_employeeId"
+        FOREIGN KEY ("employeeId") 
+            REFERENCES "Employee"("id"),
+    CONSTRAINT "FK_Order_customerId"
+        FOREIGN KEY ("customerId")
+            REFERENCES "Customer"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "OrderItem" (
+  "id" INT PRIMARY KEY,
+  "menuItemId" INT,
+  "orderId" INT,
+  "name" VARCHAR,
+  "size" VARCHAR,
+  "notes" VARCHAR,
+  "tea" VARCHAR,
+  "sugar" INT,
+  "ice" INT,
+  CONSTRAINT "FK_OrderItem_menuItemId"
+    FOREIGN KEY ("menuItemId")
+      REFERENCES "MenuItem"("id"),
+  CONSTRAINT "FK_OrderItem_orderId"
+    FOREIGN KEY ("orderId")
+      REFERENCES "Order"("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Customer" (
+  "id" INT PRIMARY KEY,
+  "email" VARCHAR,
+  "points" INT
+);
+
+CREATE TABLE IF NOT EXISTS "Employee" (
+  "id" INT PRIMARY KEY,
+  "isManager" BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS "Inventory" (
+  "id" INT PRIMARY KEY,
+  "quantity" DECIMAL,
+  "name" VARCHAR,
+);
+
+CREATE TABLE IF NOT EXISTS "Topping" (
+  "id" INT PRIMARY KEY,
+  "name" VARCHAR,
+  "price" DECIMAL
+);
+
